6b92ab46c13c4bc59eabf3e0ea1634969dcad073
MrunmaiK5 <mrunmai0205@gmail.com>
2025-11-24 16:12:32 +0530

    Added Assignment__32

---
+++ Assignments/Assignment__32/Program32_1.java
@@ -0,0 +1,67 @@
+package Assignment__32;
+
+import java.util.Scanner;
+
+class Program32_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1=0, iValue2=0;
+
+        System.out.println("Enter number of Rows: ");
+        iValue1=sobj.nextInt();
+
+        System.out.println("Enter number of Columns: ");
+        iValue2=sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  Pattern
+//  Description:    Displays a pattern of right angle triangle.
+//  Input:          Integer
+//  Output:         Void
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           23/11/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i=0,j=0;
+
+        if(iRow != iCol)
+        {
+            System.out.printf("Invalid Input\n");
+            System.out.printf("Rows and columns should be same\n");
+            return;
+        }
+
+        for(i=1; i<=iRow; i++)
+        {
+            for(j=1; j<=iCol; j++)
+            {
+                if (j == iCol-i+1) 
+                {
+                    System.out.printf("#\t");
+                }
+                else
+                {
+                    System.out.printf("*\t");
+                }
+                
+            }
+
+            System.out.printf("\n");
+        }
+    }
+}
