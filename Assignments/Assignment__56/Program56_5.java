6456bfc5d9f1d332d9aef49e784a2db1cc7060e6
MrunmaiK5 <mrunmai0205@gmail.com>
2026-01-28 20:09:12 +0530

    Added Assignment__56

---
+++ Assignments/Assignment__56/Program56_5.java
@@ -0,0 +1,64 @@
+package Assignment__56;
+
+import java.io.File;
+import java.util.Scanner;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  ListFiles
+//  Description:    Takes directory name and prints all the files names from the directory
+//  Input:          String
+//  Output:         Nothing
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           28/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Program56_5
+{
+    public static void main(String[] A) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        String DictName = "";
+
+        System.out.println("Enter Dictionary name: ");
+        DictName = sc.nextLine();
+
+        FileX fxobj = new FileX();
+        fxobj.ListFiles(DictName);
+    }
+}
+
+class FileX
+{
+    public void ListFiles(String DName)
+    {
+        String Arr[] = new String[30];
+        int iCnt = 0;
+        try 
+        {
+            
+            File fobj = new File(DName);
+
+            if (fobj.isDirectory() == true) 
+            {
+                Arr = fobj.list();
+                System.out.println("Files: ");
+                for (iCnt = 0; iCnt < Arr.length; iCnt++) 
+                {
+                    System.out.println(Arr[iCnt]);
+                }
+            }
+            else
+            {
+                System.out.println("No such directory exist");
+            }
+
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Something went wrong!!");
+        }
+    }
+}
